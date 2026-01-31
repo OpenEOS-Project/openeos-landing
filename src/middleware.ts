9f3b63ce747d85dc165326c7962e355d81888e6a
hermanntoast <lukas.spitznagel@netzint.de>
2026-01-31 13:09:46 +0100

    fix redirect error

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,5 +8,5 @@ export default createMiddleware({
 });
 
 export const config = {
-  matcher: ['/', '/(de|en)/:path*', '/((?!_next|_vercel|.*\\..*).*)'],
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
 };
